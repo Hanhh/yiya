acbc828424203e2c3f9302bae8ca04949653aca3
hanhh <hanjiaojiao@mininglamp.com>
2021-10-22 14:02:46 +0800

    fix: fix changelog overwriting

--- src/util/changelog-generate.ts
+++ src/util/changelog-generate.ts
@@ -2,11 +2,21 @@ import conventionalChangelog from "conventional-changelog";
 import fs from "fs";
 import path from "path";
 
-export const changelogGenerate = () => {
+export const changelogGenerate = async () => {
   const filePath = path.resolve(process.cwd(), "CHANGELOG.md");
   const changelog = conventionalChangelog({
     preset: "angular",
   });
-  const writerStream = fs.createWriteStream(filePath);
-  changelog.pipe(writerStream);
+  const currentFileStr = fs.readFileSync(filePath, "utf8");
+  const res = await streamToBuffer(changelog);
+  fs.writeFileSync(filePath, `${res.toString()}\n\n${currentFileStr}`);
+};
+
+const streamToBuffer = (stream): Promise<Buffer> => {
+  return new Promise((resolve, reject) => {
+    let buffers = [];
+    stream.on("error", reject);
+    stream.on("data", (data) => buffers.push(data));
+    stream.on("end", () => resolve(Buffer.concat(buffers)));
+  });
 };
